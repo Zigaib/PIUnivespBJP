9e9c03830bdb8cddf23f5185d9108d8980abd1e5
PIUnivespBJP <piunivespgrupobjp@gmail.com>
2022-05-02 20:44:27 -0300

    Atualização 02-05-22

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,92 +1,93 @@
 /*!
-* Start Bootstrap - Business Casual v7.0.5 (https://startbootstrap.com/theme/business-casual)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Business Casual v7.0.5 (https://startbootstrap.com/theme/business-casual)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
+ */
 // Highlights current date on contact page
 window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
 
-$(document).ready(function(){
-	$('[data-toggle="tooltip"]').tooltip();
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
 });
 
-$('#myCarousel').on('slide.bs.carousel', function () {
+$('#myCarousel').on('slide.bs.carousel', function() {
     // do something…
-  })
+})
 
-<script>
-    
+<
+script >
+
     function limpa_formulário_cep() {
-            //Limpa valores do formulário de cep.
-            document.getElementById('rua').value=("");
-            document.getElementById('bairro').value=("");
-            document.getElementById('cidade').value=("");
-            document.getElementById('uf').value=("");
-            
+        //Limpa valores do formulário de cep.
+        document.getElementById('rua').value = ("");
+        document.getElementById('bairro').value = ("");
+        document.getElementById('cidade').value = ("");
+        document.getElementById('uf').value = ("");
+
     }
 
-    function meu_callback(conteudo) {
-        if (!("erro" in conteudo)) {
-            //Atualiza os campos com os valores.
-            document.getElementById('rua').value=(conteudo.logradouro);
-            document.getElementById('bairro').value=(conteudo.bairro);
-            document.getElementById('cidade').value=(conteudo.localidade);
-            document.getElementById('uf').value=(conteudo.uf);
-            
-        } //end if.
-        else {
-            //CEP não Encontrado.
-            limpa_formulário_cep();
-            alert("CEP não encontrado.");
-        }
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        document.getElementById('rua').value = (conteudo.logradouro);
+        document.getElementById('bairro').value = (conteudo.bairro);
+        document.getElementById('cidade').value = (conteudo.localidade);
+        document.getElementById('uf').value = (conteudo.uf);
+
+    } //end if.
+    else {
+        //CEP não Encontrado.
+        limpa_formulário_cep();
+        alert("CEP não encontrado.");
     }
-    
+}
 
-    function pesquisacep(valor) {
 
-        //Nova variável "cep" somente com dígitos.
-        var cep = valor.replace(/\D/g, '');
+function pesquisacep(valor) {
 
-        //Verifica se campo cep possui valor informado.
-        if (cep != "") {
+    //Nova variável "cep" somente com dígitos.
+    var cep = valor.replace(/\D/g, '');
 
-            //Expressão regular para validar o CEP.
-            var validacep = /^[0-9]{8}$/;
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
 
-            //Valida o formato do CEP.
-            if(validacep.test(cep)) {
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
 
-                //Preenche os campos com "..." enquanto consulta webservice.
-                document.getElementById('rua').value="...";
-                document.getElementById('bairro').value="...";
-                document.getElementById('cidade').value="...";
-                document.getElementById('uf').value="...";
-                
+        //Valida o formato do CEP.
+        if (validacep.test(cep)) {
 
-                //Cria um elemento javascript.
-                var script = document.createElement('script');
+            //Preenche os campos com "..." enquanto consulta webservice.
+            document.getElementById('rua').value = "...";
+            document.getElementById('bairro').value = "...";
+            document.getElementById('cidade').value = "...";
+            document.getElementById('uf').value = "...";
 
-                //Sincroniza com o callback.
-                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
 
-                //Insere script no documento e carrega o conteúdo.
-                document.body.appendChild(script);
+            //Cria um elemento javascript.
+            var script = document.createElement('script');
 
-            } //end if.
-            else {
-                //cep é inválido.
-                limpa_formulário_cep();
-                alert("Formato de CEP inválido.");
-            }
+            //Sincroniza com o callback.
+            script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback';
+
+            //Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
+
         } //end if.
         else {
-            //cep sem valor, limpa formulário.
+            //cep é inválido.
             limpa_formulário_cep();
+            alert("Formato de CEP inválido.");
         }
-    };
+    } //end if.
+    else {
+        //cep sem valor, limpa formulário.
+        limpa_formulário_cep();
+    }
+};
 
 //api para aumentar a letra
 
@@ -114,31 +115,33 @@ window.onload = function() {
 
 //Linux
 // Início do código de Aumentar/ Diminuir a letra
- 
+
 // Para usar coloque o comando:
 // "javascript:mudaTamanho('tag_ou_id_alvo', -1);" para diminuir
 // e o comando "javascript:mudaTamanho('tag_ou_id_alvo', +1);" para aumentar
- 
+
 var tagAlvo = new Array('p'); //pega todas as tags p//
- 
+
 // Especificando os possíveis tamanhos de fontes, poderia ser: x-small, small...
-var tamanhos = new Array( '9px','10px','11px','12px','13px','14px','15px' );
+var tamanhos = new Array('9px', '10px', '11px', '12px', '13px', '14px', '15px');
 var tamanhoInicial = 2;
- 
-function mudaTamanho( idAlvo,acao ){
-if (!document.getElementById) return
-var selecionados = null,tamanho = tamanhoInicial,i,j,tagsAlvo;
-tamanho += acao;
-if ( tamanho < 0 ) tamanho = 0;
-if ( tamanho > 6 ) tamanho = 6;
-tamanhoInicial = tamanho;
-if ( !( selecionados = document.getElementById( idAlvo ) ) ) selecionados = document.getElementsByTagName( idAlvo )[ 0 ];
- 
-selecionados.style.fontSize = tamanhos[ tamanho ];
- 
-for ( i = 0; i < tagAlvo.length; i++ ){
-tagsAlvo = selecionados.getElementsByTagName( tagAlvo[ i ] );
-for ( j = 0; j < tagsAlvo.length; j++ ) tagsAlvo[ j ].style.fontSize = tamanhos[ tamanho ];
-}
+
+function mudaTamanho(idAlvo, acao) {
+    if (!document.getElementById) return
+    var selecionados = null,
+        tamanho = tamanhoInicial,
+        i, j, tagsAlvo;
+    tamanho += acao;
+    if (tamanho < 0) tamanho = 0;
+    if (tamanho > 6) tamanho = 6;
+    tamanhoInicial = tamanho;
+    if (!(selecionados = document.getElementById(idAlvo))) selecionados = document.getElementsByTagName(idAlvo)[0];
+
+    selecionados.style.fontSize = tamanhos[tamanho];
+
+    for (i = 0; i < tagAlvo.length; i++) {
+        tagsAlvo = selecionados.getElementsByTagName(tagAlvo[i]);
+        for (j = 0; j < tagsAlvo.length; j++) tagsAlvo[j].style.fontSize = tamanhos[tamanho];
+    }
 }
 // Fim do código de Aumentar/ Diminuir a letra

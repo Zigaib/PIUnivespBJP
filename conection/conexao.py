b6e0c5ced7193901a0b3de99d297df183205df72
zameethi <andre.zigaib@gmail.com>
2022-12-03 21:08:26 -0300

    update

---
+++ conection/conexao.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy as sa
+from flask import Flask
+import os
+
+app = Flask(__name__, template_folder=os.path.abspath('templates'), static_folder=os.path.abspath('static'), static_url_path='/static')
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db_clientes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SECRET_KEY'] = '@ALKSjsjhfdh%$fdbdh'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db=sa(app)
+
+db.Model.metadata.reflect(db.engine)
+
+

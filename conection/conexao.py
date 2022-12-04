2c1c94d7bc8670d3804cd7285990dc8e56f29817
zameethi <andre.zigaib@gmail.com>
2022-12-04 00:07:23 -0300

    Update

--- conection/conexao.py
+++ conection/conexao.py
@@ -8,7 +8,6 @@ load_dotenv()
 app = Flask(__name__, template_folder=os.path.abspath('templates'), static_folder=os.path.abspath('static'), static_url_path='/static')
 
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db_clientes.db'
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['SECRET_KEY'] = '@ALKSjsjhfdh%$fdbdh'

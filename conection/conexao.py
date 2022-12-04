6cf0c363db3bc4987c07f6bb0434eb53f0c8a171
PIUNIVESP2022 <94024429+PIUnivespBJP@users.noreply.github.com>
2022-12-03 21:38:44 -0300

    Add files via upload

--- conection/conexao.py
+++ conection/conexao.py
@@ -5,7 +5,9 @@ import os
 app = Flask(__name__, template_folder=os.path.abspath('templates'), static_folder=os.path.abspath('static'), static_url_path='/static')
 
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db_clientes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['SECRET_KEY'] = '@ALKSjsjhfdh%$fdbdh'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db=sa(app)
 
